67e2b866c7b9b6ae353872c27c1570a98f29b6bb
sebastian461 <sebastiano-461@hotmail.com>
2024-02-06 16:34:24 -0500

    Migrations

--- database/migrations/2024_02_06_213020_create_user_events_table.php
+++ database/migrations/2024_02_06_213020_create_user_events_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
    */
   public function up(): void
   {
-    Schema::create('users', function (Blueprint $table) {
+    Schema::create('user_events', function (Blueprint $table) {
       $table->id();
-      $table->string('name');
-      $table->string('email')->unique();
-      $table->timestamp('email_verified_at')->nullable();
-      $table->string('password');
-      $table->rememberToken();
+      $table->unsignedBigInteger("user_id");
+      $table->unsignedBigInteger("event_id");
+      $table->foreign('user_id')->references('id')->on('users');
+      $table->foreign('event_id')->references('id')->on('events');
       $table->timestamps();
     });
   }
@@ -27,6 +26,6 @@ return new class extends Migration
    */
   public function down(): void
   {
-    Schema::dropIfExists('users');
+    Schema::dropIfExists('user_events');
   }
 };
